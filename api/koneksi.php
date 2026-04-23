360333e9666b29327e400a8be3012dd105423df9
zzahrina455-design <zzahrina455@gmail.com>
2026-04-23 14:09:26 +0700

    up

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,15 +1,30 @@
 <?php
-$host = "127.0.0.1";
-$user = "root";
-$pass = "";
-$db   = "sistem_klinik";
-$port = 3306; 
+// Data dari TiDB Cloud
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = '42DMDXTL42WVaa2.root';
+$pass = 'ppxrISmOZg7Vac1Y';
+$db   = 'sistem_klinik';
 
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+// Inisialisasi mysqli
+$koneksi = mysqli_init();
 
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
 }
-
-mysqli_set_charset($conn, "utf8mb4");
 ?>
