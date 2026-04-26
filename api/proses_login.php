18a45d2ebb7b1d43b5164c93dee2c32d4bd65bb5
zzahrina455-design <zzahrina455@gmail.com>
2026-04-26 23:10:54 +0700

    up

--- api/proses_login.php
+++ api/proses_login.php
@@ -7,11 +7,9 @@ if (isset($_POST['login'])) {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = $_POST['password'];
 
-    // Cari user di database berdasarkan username
     $query = "SELECT * FROM tbl_user WHERE username='$username'";
     $result = mysqli_query($conn, $query);
 
-    // Cek apakah usernamenya ada
     if (mysqli_num_rows($result) === 1) {
         $data = mysqli_fetch_assoc($result);
 
@@ -21,20 +19,21 @@ if (isset($_POST['login'])) {
             $_SESSION['username'] = $data['username'];
             $_SESSION['role'] = $data['role'];
 
-            // REDIRECT BERDASARKAN ROLE
+            // REDIRECT FIX
             if ($data['role'] == 'admin') {
-                echo "<script>alert('Login Berhasil sebagai Admin!'); window.location='dashboard_admin.php';</script>";
+                header("Location: dashboard_admin.php");
             } else {
-                echo "<script>alert('Login Berhasil sebagai User!'); window.location='dashboard_user.php';</script>";
+                header("Location: dashboard_user.php");
             }
             exit;
 
         } else {
-            echo "<script>alert('Password salah!'); window.location='login.php';</script>";
+            header("Location: login.php?error=password");
+            exit;
         }
     } else {
-        // Username tidak ditemukan
-        echo "<script>alert('Username tidak terdaftar!'); window.location='login.php';</script>";
+        header("Location: login.php?error=username");
+        exit;
     }
 } else {
     header("Location: login.php");
