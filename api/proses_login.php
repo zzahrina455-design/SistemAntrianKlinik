4731b5cc6cf2a55e7ba681e5eeb335024738f317
zzahrina455-design <zzahrina455@gmail.com>
2026-04-26 22:23:12 +0700

    up

--- api/proses_login.php
+++ api/proses_login.php
@@ -1,43 +1,48 @@
 <?php
 include 'koneksi.php';
 
-// Ambil data dari form
-$username = $_POST['username'];
-$password = $_POST['password'];
+if (isset($_POST['login'])) {
+   
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = $_POST['password'];
 
-// Cek ke database
-$query = "SELECT * FROM tbl_user WHERE username='$username'";
-$result = mysqli_query($conn, $query);
+    // Cari user di database
+    $query = "SELECT * FROM tbl_user WHERE username = '$username'";
+    $result = mysqli_query($conn, $query);
 
-// Cek apakah user ada
-if (mysqli_num_rows($result) > 0) {
-    $data = mysqli_fetch_assoc($result);
+    if (mysqli_num_rows($result) === 1) {
+        $data = mysqli_fetch_assoc($result);
 
-    // Verifikasi password
-    if (password_verify($password, $data['password'])) {
+        if (password_verify($password, $data['password'])) {
 
-        // ✅ AMBIL DATA DARI DATABASE
-        $id = $data['id'];
-        $role = $data['role'];
-        $username = $data['username'];
+            // ✅ AMBIL DATA DARI DATABASE
+            $id = $data['id'];
+            $role = $data['role'];
+            $username = $data['username'];
 
-        // ✅ SET COOKIE
-        setcookie("id", $id, time() + 3600, "/");
-        setcookie("user", $username, time() + 3600, "/");
-        setcookie("role", $role, time() + 3600, "/");
+            // ✅ SET COOKIE
+            setcookie("user", $username, time() + 3600, "/");
+            setcookie("role", $role, time() + 3600, "/");
+            setcookie("id", $id, time() + 3600, "/");
 
-        // ✅ REDIRECT SESUAI ROLE
-        if ($role == 'admin') {
-            header("Location: dashboard_admin.php");
+            // ✅ REDIRECT
+            if ($role == 'admin') {
+                header("Location: dashboard_admin.php");
+            } else {
+                header("Location: dashboard_user.php");
+            }
+            exit();
+
         } else {
-            header("Location: dashboard_user.php");
+            echo "<script>alert('Password salah!'); window.location='login.php';</script>";
         }
-        exit();
 
     } else {
-        echo "Password salah!";
+        echo "<script>alert('Username tidak terdaftar!'); window.location='login.php';</script>";
     }
 
 } else {
-    echo "Username tidak ditemukan!";
+    header("Location: login.php");
+    exit;
 }
+?>
