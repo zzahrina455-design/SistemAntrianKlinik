77ff6fdef1a88fa1ce1e9f1ff34b3e7166a43757
zzahrina455-design <zzahrina455@gmail.com>
2026-04-27 17:04:00 +0700

    up

--- api/dashboard_user.php
+++ api/dashboard_user.php
@@ -27,7 +27,7 @@ $tiket_aktif = mysqli_fetch_assoc($result);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../style_user.css">
+    <link rel="stylesheet" href="api/style_user.css">
 </head>
 <body>
     <nav class="navbar navbar-dark shadow-sm py-3">
