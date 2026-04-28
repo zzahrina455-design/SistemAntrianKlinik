d818e5a3df3632cd1d80d8a712222f1c7799e729
zzahrina455-design <zzahrina455@gmail.com>
2026-04-29 00:17:12 +0700

    up

--- api/proses_register.php
+++ api/proses_register.php
@@ -6,9 +6,22 @@ if (isset($_POST['register'])) {
     $email    = mysqli_real_escape_string($conn, $_POST['email']);
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     
-    // Paksa role jadi admin
-    $role = 'admin';
-    
+    // Ambil domain email
+    $domain = substr(strrchr($email, "@"), 1);
+
+    // Tentukan role berdasarkan domain
+    if ($domain == "gmail.com") {
+        $role = "user";
+    } elseif ($domain == "admin.com") {
+        $role = "admin";
+    } else {
+        echo "<script>
+                alert('Email harus menggunakan @gmail.com atau @admin.com!');
+                window.history.back();
+              </script>";
+        exit();
+    }
+
     $password_raw = $_POST['password'];
     $password_hashed = password_hash($password_raw, PASSWORD_DEFAULT);
 
@@ -17,7 +30,7 @@ if (isset($_POST['register'])) {
     
     if (mysqli_query($conn, $query)) {
         echo "<script>
-                alert('Registrasi Admin Berhasil!');
+                alert('Registrasi Berhasil sebagai $role!');
                 window.location='login.php';
               </script>";
     } else {
